af17ada90d28ec22aee6ab16252a8e894f958353
quinthemint <quinn.potter93@gmail.com>
2024-04-11 13:56:30 -0700

    find users by name added

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,12 +40,25 @@ const users = {
       },
       {
         id: "zap555",
-        name: "Dennis",
+        name: "James",
         job: "Bartender"
       }
     ]
   };
 
+  const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+  
   app.get("/users", (req, res) => {
-    res.send(users);
+    const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
   });
